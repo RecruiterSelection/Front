e2b85461639b7bee1a989bb1314136b689821852
silviacarvalhodacunha <silviacarvalhodacunha@gmail.com>
2023-11-16 14:56:36 -0300

    feat: providers user

---
+++ src/providers/user/userProvider.tsx
@@ -0,0 +1,107 @@
+import { createContext, useState } from "react";
+import {
+  IDefaultProviderProps,
+  ILoginUser,
+  IRegisterUser,
+  IUser,
+  IUserContext,
+  TUpdateUser,
+} from "./interface";
+import { api } from "../../services/api";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+
+export const UserContext = createContext({} as IUserContext);
+
+export const UserProvider = ({ children }: IDefaultProviderProps) => {
+  const [user, setUser] = useState<IUser>({} as IUser);
+
+  const navigate = useNavigate();
+
+  const createUser = async (data: IRegisterUser) => {
+    await api
+      .post("/users", data)
+      .then((res) => {
+        setUser(res.data);
+        toast.success("Perfil criado com succeso!");
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const getUser = async () => {
+    await api
+      .get(`/users`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("@TOKEN")}`,
+        },
+      })
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const updateUser = async (data: TUpdateUser) => {
+    await api
+      .patch(`/users/${user.id}`, data, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("@TOKEN")}`,
+        },
+      })
+      .then((res) => {
+        setUser(res.data);
+        getUser();
+        toast.success("Perfil editado com succeso!");
+      })
+      .catch((err) => toast.error(err));
+  };
+
+  const deleteUser = async () => {
+    await api
+      .delete(`/users/${user.id}`)
+      .then(() => toast.success("Perfil deletado com sucesso"))
+      .catch((err) => console.error(err));
+  };
+
+  const loginUser = async (data: ILoginUser) => {
+    await api
+      .post("", data)
+      .then((res) => {
+        localStorage.setItem("@TOKEN", res.data.token);
+        getUser();
+        toast.success("Login realizado com sucesso!");
+        setTimeout(() => {
+          navigate("/DashBoard");
+        }, 3000);
+      })
+      .catch((err) => {
+        toast.error("Email ou Senha incorreta");
+        console.error(err);
+      });
+  };
+
+  const logoutUser = () => {
+    setUser({} as IUser);
+    localStorage.removeItem("@TOKEN");
+    setTimeout(() => {
+      navigate("/");
+    }, 3000);
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        createUser,
+        getUser,
+        updateUser,
+        deleteUser,
+        loginUser,
+        logoutUser,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
